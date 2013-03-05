93c7d8d734d619950c8168c8fb46ca94c94de936
Tomas Zulberti <tzulberti@gmail.com>
2013-03-05 16:57:15 -0300

    Updated import data and added to use any host

--- flask_pypi_proxy/views/__init__.py
+++ flask_pypi_proxy/views/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from flask_pypi.app import app
-import flask_pypi.views.package
-import flask_pypi.views.pypi
-import flask_pypi.views.simple
+from flask_pypi_proxy.app import app
+import flask_pypi_proxy.views.package
+import flask_pypi_proxy.views.pypi
+import flask_pypi_proxy.views.simple
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
